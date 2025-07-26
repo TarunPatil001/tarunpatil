c873214a1a28919d04566d03986203f433317625
Tarun Patil <tarunpatil001@gmail.com>
2025-07-26 23:43:07 +0530

    chore: Initial commit
        - Add initial project files
        - Include LICENSE, README, assets, CSS, HTML, and JS files

---
+++ js/data.js
@@ -0,0 +1,26 @@
+window.apiData = {
+  about: [
+    {
+      fullname: "Tarun Patil",
+      description: "Aspiring Full Stack Java Developer and B.E. in Computer Engineering",
+    }
+  ],
+  skills: [
+    "Java", "Spring Boot", "HTML", "CSS", "JavaScript", "Bootstrap"
+  ],
+  projects: [
+    {
+      title: "Portfolio Website",
+      description: "A creative developer portfolio inspired by Google Search.",
+      tech: ["HTML", "CSS", "JavaScript"],
+      link: "https://your-netlify-domain.netlify.app/"
+    },
+    // Add more projects if needed
+  ],
+  overview: [
+    { url: "https://images.pexels.com/photos/1549974/pexels-photo-1549974.jpeg?_gl=1*1vyu2x0*_ga*MTg1MzUwMzg0My4xNzQ5NTUyMjA0*_ga_8JE65Q40S6*czE3NTMyMTcyNDEkbzE0JGcxJHQxNzUzMjE3Mjk1JGo2JGwwJGgw" },
+    { url: "https://images.pexels.com/photos/1549974/pexels-photo-1549974.jpeg?_gl=1*1vyu2x0*_ga*MTg1MzUwMzg0My4xNzQ5NTUyMjA0*_ga_8JE65Q40S6*czE3NTMyMTcyNDEkbzE0JGcxJHQxNzUzMjE3Mjk1JGo2JGwwJGgw" },
+    { url: "https://images.pexels.com/photos/1549974/pexels-photo-1549974.jpeg?_gl=1*1vyu2x0*_ga*MTg1MzUwMzg0My4xNzQ5NTUyMjA0*_ga_8JE65Q40S6*czE3NTMyMTcyNDEkbzE0JGcxJHQxNzUzMjE3Mjk1JGo2JGwwJGgw" },
+    { url: "https://images.pexels.com/photos/1549974/pexels-photo-1549974.jpeg?_gl=1*1vyu2x0*_ga*MTg1MzUwMzg0My4xNzQ5NTUyMjA0*_ga_8JE65Q40S6*czE3NTMyMTcyNDEkbzE0JGcxJHQxNzUzMjE3Mjk1JGo2JGwwJGgw" }
+  ],
+};
